b04f67b576187b2d079490746f8bf6fd1f7da6a3
mscheske13 <145404490+mscheske13@users.noreply.github.com>
2024-08-28 10:54:07 -0400

    Update NCAAMpy.py

--- NCAAMpy.py
+++ NCAAMpy.py
@@ -1,42 +1,42 @@
-from Teams_and_players import get_player, get_team, get_schedule, get_coach, get_roster
-from day_scraper import get_day
-from Game_Scraper import make_game, game_stats
+import Teams_and_players as tp
+import day_scraper as ds
+import Game_Scraper as gs
 import time
 
 
 def scrape_game(game_id):
     time.sleep(2)
-    return make_game(game_id)
+    return gs.make_game(game_id)
 
 
 def scrape_box(game_id):
     time.sleep(2)
-    return game_stats(game_id)
+    return gs.game_stats(game_id)
 
 
 def scrape_player(player_id):
     time.sleep(2)
-    return get_player(player_id)
+    return tp.get_player(player_id)
 
 
 def scrape_schedule(schedule_id):
     time.sleep(2)
-    return get_schedule(schedule_id)
+    return tp.get_schedule(schedule_id)
 
 
 def scrape_team(team_id):
     time.sleep(2)
-    return get_team(team_id)
+    return tp.get_team(team_id)
 
 
 def scrape_coach(coach_id):
     time.sleep(2)
-    return get_coach(coach_id)
+    return tp.get_coach(coach_id)
 
 
 def scrape_roster(roster_id):
     time.sleep(2)
-    return get_roster(roster_id)
+    return tp.get_roster(roster_id)
 
 
 def scrape_day(date, conference_id='0', tournament_id='', division=1, w=False, season_id=None):
